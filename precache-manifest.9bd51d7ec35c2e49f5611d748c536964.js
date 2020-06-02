9739c041ed883821d56929a5aa4b43aec1a103d1
ClassicalJo <jb.barrientos@yahoo.com>
2020-06-02 19:12:57 +1000

    Updates

---
+++ precache-manifest.9bd51d7ec35c2e49f5611d748c536964.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78016b6da744b890d88ce46786fde4a",
+    "url": "/mage-js/index.html"
+  },
+  {
+    "revision": "b42aa348fb3413a51146",
+    "url": "/mage-js/static/css/main.3710abfe.chunk.css"
+  },
+  {
+    "revision": "55378720e0d2103d6dae",
+    "url": "/mage-js/static/js/2.b26ab327.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/mage-js/static/js/2.b26ab327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b42aa348fb3413a51146",
+    "url": "/mage-js/static/js/main.e419675b.chunk.js"
+  },
+  {
+    "revision": "3a737d50478fd22634c5",
+    "url": "/mage-js/static/js/runtime-main.9b983c2f.js"
+  }
+]);
